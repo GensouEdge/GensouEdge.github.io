262bdbc9d44d1ea4e7a878b1ee0e0a83c650f45a
Flora <2054947263@qq.com>
2025-12-16 11:29:39 +0800

    删除js/htmlRouter.js

--- js/htmlRouter.js
+++
@@ -1,7 +0,0 @@
-let currentUrl = window.location.href;
-
-if (currentUrl.endsWith('.html')) {
-    let newUrl = currentUrl.slice(0, -5);
-    window.history.replaceState(null, '', newUrl);
-    // 这实现太他妈蠢了，我也不想用
-}
